c3b80acb9fb4e157056963cb0b0dd02f8e192523
Shivam Tiwari <shivam4819@gmail.com>
2022-11-03 14:06:38 +0530

    Create addFunction.js

---
+++ web3_interaction/functions/addFunction.js
@@ -0,0 +1,17 @@
+const {createInstance} = require('./connection.js')
+
+async function addNumbers(num1, num2){
+    const {instance, accounts} = await createInstance();
+
+    //this txn emits event. From contract we are returning the result in event hence to access the value
+    // console res.events.event_name.returnValues.param_name
+    const res = await instance.methods.addNum(num1, num2).send({from: accounts[0] });
+    console.log("Event value:", res.events.success.returnValues.value);
+
+    // if you want to just receive the return value then use .call method
+    // const res = await instance.methods.addNum(num1, num2).call({from: accounts[0] });
+
+    console.log(res);
+}
+
+addNumbers(1, 2);
